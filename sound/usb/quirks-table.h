26b415827c601382ff10543e3f26540930137f48
Ben graham <64079735+ben443@users.noreply.github.com>
2026-06-18 18:25:39 +1000

    Update quirks-table.h

--- sound/usb/quirks-table.h
+++ sound/usb/quirks-table.h
@@ -470,6 +470,32 @@ YAMAHA_DEVICE(0x7010, "UB99"),
 				}
 			},
 			{
+	/* BOSS GT-1 */
+	USB_DEVICE(0x0582, 0x01d6),
+	.driver_info = (unsigned long) & (const struct snd_usb_audio_quirk) {
+		.ifnum = QUIRK_ANY_INTERFACE,
+		.type = QUIRK_COMPOSITE,
+		.data = (const struct snd_usb_audio_quirk[]) {
+			{
+				.ifnum = 0,
+				.type = QUIRK_AUDIO_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = 1,
+				.type = QUIRK_AUDIO_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = 2,
+				.type = QUIRK_MIDI_STANDARD_INTERFACE
+			},
+			{
+				.ifnum = -1
+			}
+		}
+	}
+},
+
+   {
 				.ifnum = 1,
 				.type = QUIRK_AUDIO_FIXED_ENDPOINT,
 				.data = & (const struct audioformat) {
